b969cf9caca0f3e2e173a5bfe0c708f55e22deda
colecharb <40375497+colecharb@users.noreply.github.com>
2025-02-04 11:28:53 -0500

    fix(new recipe): fixes theming issues from migration

--- app/new-recipe.tsx
+++ app/new-recipe.tsx
@@ -19,145 +19,121 @@ export default function NewRecipeScreen() {
   const [recipeIngredients, setRecipeIngredients] = useState<string[]>(['']);
   const [recipeMethod, setRecipeMethod] = useState<string[]>(['']);
 
-  const setIngredient = (index: number) => (
-    (ingredient: string) => {
-      setRecipeIngredients(
-        (prev) => ([
-          ...prev.slice(0, index),
-          ingredient,
-          ...prev.slice(index + 1),
-        ])
-      );
-    }
-  );
+  const setIngredient = (index: number) => (ingredient: string) => {
+    setRecipeIngredients((prev) => [...prev.slice(0, index), ingredient, ...prev.slice(index + 1)]);
+  };
 
-  const setStep = (index: number) => (
-    (step: string) => {
-      setRecipeMethod(
-        (prev) => ([
-          ...prev.slice(0, index),
-          step,
-          ...prev.slice(index + 1),
-        ])
-      );
-    }
-  );
+  const setStep = (index: number) => (step: string) => {
+    setRecipeMethod((prev) => [...prev.slice(0, index), step, ...prev.slice(index + 1)]);
+  };
 
   useSetOptions({ title: recipeName });
 
-  const onPressSave = () => (
+  const onPressSave = () =>
     saveRecipe({
       id: uuid(),
       name: recipeName,
       ingredients: recipeIngredients,
       method: recipeMethod,
-    })
-  );
+    });
 
   const styles = useStyles();
 
   return (
-    <KeyboardAvoidingView
-      behavior='height'
-      style={styles.keyboardAvoidingView}
-    >
-      <SafeAreaScrollView
-        keyboardShouldPersistTaps='handled'
-        contentContainerStyle={styles.container}
+    <View style={styles.container}>
+      <KeyboardAvoidingView
+        behavior='height'
+        style={styles.keyboardAvoidingView}
       >
-
-        <View style={styles.section}>
-          <TextInput
-            placeholder='Recipe Name'
-            autoCapitalize='words'
-            style={styles.heading}
-            onChangeText={setRecipeName}
-          />
-        </View>
-        <View style={styles.section}>
-          <Text style={styles.heading}>
-            Ingredients
-          </Text>
-          <View style={styles.sectionContent}>
-            {
-              recipeIngredients.map(
-                (ingredient, index) => (
-                  <View
-                    key={index}
-                    style={styles.ingredientRow}
-                  >
-                    <Text style={styles.sectionText}>
-                      •
-                    </Text>
-                    <TextInput
-                      placeholder='Add ingredient...'
-                      style={[styles.sectionText, { flex: 1 }]}
-                      onChangeText={(text) => setIngredient(index)(text)}
-                      value={ingredient}
-                    />
-                  </View>
-                )
-              )
-            }
-            <Button
-              title='add'
-              onPress={() => setRecipeIngredients((prev) => [...prev, ''])}
+        <SafeAreaScrollView
+          keyboardShouldPersistTaps='handled'
+          contentContainerStyle={styles.safeAreaScrollView}
+        >
+          <View style={styles.section}>
+            <TextInput
+              autoFocus
+              placeholder='Recipe Name'
+              autoCapitalize='words'
+              style={styles.heading}
+              onChangeText={setRecipeName}
             />
           </View>
-        </View>
-
-        <View style={styles.section}>
-          <Text style={styles.heading}>
-            Preparation
-          </Text>
-          <View style={styles.sectionContent}>
-            {
-              recipeMethod.map(
-                (step, index) => (
-                  <View
-                    key={index}
-                    style={styles.ingredientRow}
-                  >
-                    <Text style={styles.sectionText}>
-                      •
-                    </Text>
-                    <TextInput
-                      placeholder='Add step...'
-                      style={[styles.sectionText, { flex: 1 }]}
-                      onChangeText={(text) => setStep(index)(text)}
-                      value={step}
-                    />
-                  </View>
-                )
-              )
-            }
-            <Button
-              title='add'
-              onPress={() => setRecipeMethod((prev) => [...prev, ''])}
-            />
+          <View style={styles.section}>
+            <Text style={styles.heading}>Ingredients</Text>
+            <View style={styles.sectionContent}>
+              {recipeIngredients.map((ingredient, index) => (
+                <View
+                  key={index}
+                  style={styles.ingredientRow}
+                >
+                  <Text style={styles.sectionText}>•</Text>
+                  <TextInput
+                    placeholder='Add ingredient...'
+                    style={[styles.sectionText, { flex: 1 }]}
+                    onChangeText={(text) => setIngredient(index)(text)}
+                    value={ingredient}
+                  />
+                </View>
+              ))}
+              <Button
+                title='add'
+                onPress={() => setRecipeIngredients((prev) => [...prev, ''])}
+              />
+            </View>
           </View>
-        </View>
 
-        <Link asChild href={'..'} replace>
-          <Button
-            title='Save'
-            onPress={onPressSave}
-          />
-        </Link>
+          <View style={styles.section}>
+            <Text style={styles.heading}>Preparation</Text>
+            <View style={styles.sectionContent}>
+              {recipeMethod.map((step, index) => (
+                <View
+                  key={index}
+                  style={styles.ingredientRow}
+                >
+                  <Text style={styles.sectionText}>•</Text>
+                  <TextInput
+                    placeholder='Add step...'
+                    style={[styles.sectionText, { flex: 1 }]}
+                    onChangeText={(text) => setStep(index)(text)}
+                    value={step}
+                  />
+                </View>
+              ))}
+              <Button
+                title='add'
+                onPress={() => setRecipeMethod((prev) => [...prev, ''])}
+              />
+            </View>
+          </View>
 
-      </SafeAreaScrollView>
-    </KeyboardAvoidingView>
+          <Link
+            asChild
+            href={'..'}
+            replace
+          >
+            <Button
+              title='Save'
+              onPress={onPressSave}
+            />
+          </Link>
+        </SafeAreaScrollView>
+      </KeyboardAvoidingView>
+    </View>
   );
 }
 
 const useStyles = () => {
-  const { layout } = useDishesTheme();
+  const { layout, colors } = useDishesTheme();
   return StyleSheet.create({
-    keyboardAvoidingView: { flex: 1 },
     container: {
+      flex: 1,
+    },
+    keyboardAvoidingView: { flex: 1 },
+    safeAreaScrollView: {
       paddingHorizontal: layout.spacer,
       paddingTop: 100 + layout.spacer,
       gap: layout.spacer * 2,
+      backgroundColor: colors.background,
     },
     recipeName: {
       fontSize: 35,
