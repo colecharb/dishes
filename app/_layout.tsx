63b92d5e878e9cd02032cfca6723772fecb694b1
colecharb <40375497+colecharb@users.noreply.github.com>
2025-04-12 20:03:06 -0400

    fix: eliminates navigation error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 // import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack, useNavigation } from 'expo-router';
+import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
@@ -82,7 +82,7 @@ function RootLayoutNav() {
           title: 'Recipes',
         }}
       />
-      <Stack.Screen name='recipe/[recipeId]' />
+      <Stack.Screen name='recipe/[recipeId]/index' />
       <Stack.Screen
         name='recipe/[recipeId]/edit'
         options={{
