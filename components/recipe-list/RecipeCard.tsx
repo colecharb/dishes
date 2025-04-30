01583bd2aa51e1fb904db8e25f73dfc887706c47
colecharb <40375497+colecharb@users.noreply.github.com>
2025-04-29 22:05:47 -0400

    refactor(recipe card): renames component and exports style

--- components/recipe-list/RecipeCard.tsx
+++ components/recipe-list/RecipeCard.tsx
@@ -1,79 +1,60 @@
 import { Recipe } from '@/constants/Recipes';
 import { Text } from 'react-native-paper';
-import { StyleSheet, Dimensions, View } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
 import { useDishesTheme } from '@/constants/Theme';
+import { View } from '../Themed';
 
-type RecipeCardProps = {
+export const CARD_HEIGHT = 150;
+export const PEEK_HEIGHT = 60;
+
+type RecipeListItemProps = {
   recipe: Recipe;
   index: number;
 };
 
-const RecipeCard = ({ recipe, index }: RecipeCardProps) => {
+const RecipeListItem = ({ recipe, index }: RecipeListItemProps) => {
   const styles = useStyles();
-  // const { width } = Dimensions.get('window');
-  // const cardWidth = width * 0.8;
-  const overlapOffset = 50; // How much each card overlaps
 
   return (
-    <Link
-      asChild
-      href={{
-        pathname: '/recipe/[recipeId]',
-        params: { recipeId: recipe.id },
-      }}
-    >
-      <View
-        style={[
-          styles.container,
-          {
-            top: index * overlapOffset,
-            // left: (width - cardWidth) / 2,
-          },
-        ]}
+    <View style={styles.container}>
+      <Link
+        asChild
+        href={{
+          pathname: '/recipe/[recipeId]',
+          params: { recipeId: recipe.id },
+        }}
       >
-        <View style={styles.content}>
+        <Pressable>
           <Text style={styles.recipeName}>{recipe.name}</Text>
-        </View>
-      </View>
-    </Link>
+        </Pressable>
+      </Link>
+    </View>
   );
 };
 
-const useStyles = () => {
+export const useStyles = () => {
   const { layout, colors } = useDishesTheme();
-  const { width } = Dimensions.get('window');
-  const cardWidth = width * 0.8;
-  const cardHeight = cardWidth * 2; // 1:2 aspect ratio
-
   return StyleSheet.create({
     container: {
-      position: 'absolute',
-      width: cardWidth,
-      height: cardHeight,
-      backgroundColor: colors.secondary,
-      borderRadius: 20,
-      borderColor: colors.primary,
-      borderWidth: layout.borderWidth,
+      padding: layout.spacer,
+      paddingTop: layout.spacer / 2,
+      backgroundColor: colors.background,
+      // borderWidth: layout.borderWidth,
+      // borderColor: colors.secondary,
+      borderRadius: layout.spacer / 2,
+      height: CARD_HEIGHT,
+      marginBottom: -(CARD_HEIGHT - PEEK_HEIGHT),
       shadowColor: colors.shadow,
-      shadowOffset: layout.shadowOffset,
+      // shadowOffset: { height: 0, width: 0 },
       shadowOpacity: layout.shadowOpacity,
-      shadowRadius: layout.shadowRadius,
-      elevation: 5,
-      overflow: 'visible',
-    },
-    content: {
-      flex: 1,
-      padding: layout.spacer * 2,
-      justifyContent: 'flex-end',
-      backgroundColor: 'transparent',
+      shadowRadius: layout.spacer / 2,
     },
     recipeName: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      color: colors.onBackground,
+      fontSize: 25,
+      fontWeight: 900,
     },
   });
 };
 
-export default RecipeCard;
+export default RecipeListItem;
