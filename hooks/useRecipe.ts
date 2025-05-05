acdee20d1a34332f6b6da94ecca43790efc0f12d
colecharb <40375497+colecharb@users.noreply.github.com>
2025-05-05 18:13:56 -0400

    chore: removes commented code

--- hooks/useRecipe.ts
+++ hooks/useRecipe.ts
@@ -6,18 +6,6 @@ import useRecipes from './useRecipes';
 const useRecipe = (recipeId: string) => {
   const { recipes } = useRecipes();
   const recipe = recipes.find((r) => r.id === recipeId);
-  // const [recipe, setRecipe] = useState<Recipe | null>();
-
-  // const loadRecipe = useCallback(
-  //   () => RecipeStorage.get(recipeId).then((recipe) => setRecipe(recipe)),
-  //   [recipeId],
-  // );
-
-  // useEffect(() => {
-  //   loadRecipe();
-  // }, [loadRecipe]);
-
-  // const reloadRecipe = loadRecipe;
 
   return [recipe];
 };
